b6d2490dc6e94e0e3dc1f6f19d1adfb8c5762626
soumya3202 <152801465+soumya3202@users.noreply.github.com>
2023-12-08 20:28:47 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -72,7 +72,283 @@ class Not(Sentence):
       return hash(("not",hash(self.operand)))
 
     def__repr__(self):
-       return  hash(("not",hash(self.
+       return  f"Not({self.operand})"
+     def evaluate(self,model):
+         return not self.operand.evaluate(model)
+
+     def formula(self):
+         return "or"+Sentence.parenthesize(self.operand.formula())
+     def symbols(self):
+         return self.operand.symbols()
+
+class And(Sentence):
+    def__init__(self,*conjuncts):
+
+          for conjunct in conjuncts:
+              Sentence.validate(conjuncts)
+          self.conjuncts=list(conjuncts)
+    def__eq__(self,other):
+         return isintance(other,And) and self.conjuncts ==other.conjuncts
+
+    def__hash__(self):
+         return hash(
+         ("and",tuple(hash(conjunct)for conjunct in self.conjuncts))
+         )
+
+     def__repr__(self):
+          conjunctions=",".join(
+              [str(conjunct) for conjunct in self.conjuncts]
+          )
+          return f"And({conjunctions})"
+
+     def add(self,conjunct):
+         Sentence.validate(conjunct)
+         self.conjuncts.append(conjunct)
+       def evaluate(self,model):
+           return all(conjunct.evaluate(model) for conjunct in self.conjuncts)
+
+       def formula(self):
+           if len(self.conjuncts)==1:
+               return self.conjuncts[0].formula()
+            return "^".join[Sentence.parenthesize(conjunct.formula())
+                           for conjunct in self.conjuncts])
+       def symbols(self):
+           return set.union(*[conjunct.symbols() for conjunct in self.conjuncts])
+
+
+class Or(Sentence):
+    def __init__(self,*disjuncts):
+        for disjunct in disjuncts):
+            Sentence.validate(disjunct)
+    self.disjuncts=list(disjuncts)
+    def __eq__(self,other):
+        return isinstance(other,Or) nd self.disjuncts  == other.disjuncts
+     def __hash__(self):
+         return hash(
+             ("or",tuple(hash(disjunct) for disjunct in self.disjuncts)))
+          )
+      def __repr__(self):
+          disjuncts=",".join([str(disjunct) for disjunct in self.disjuncts])
+          return f"Or({disjuncts})
+
+     def evaluate(self,model):
+         return any(disjunct.evaluate(model) for disjunct in self.disjuncts)
+
+    def formula(self):
+         if len(self.disjuncts)==1:
+            return self.disjuncts[0].formula()
+         return "V".join([Sentence.parenthesize(disjunct.formula())
+                         for disjunct in self.disjuncts])
+
+      def symbols(self):
+           return set.union(*[disjunct.symbols() for disjunct in self.disjuncts])
+
+class  Implication(Sentence):
+       def __init__(self,antecedent,consequent):
+
+            Sentence.validate(antecedent)
+            Sentence.validate(consequent)
+            self.antecedent=antecedent
+             self.consequent=consequent
+
+         def __eq__(self,other):
+             return (isinstance(other,Implication)
+                    and self.antecedent == other.antecedent
+                    and self.consequent == other.consequent)
+         def __hash__(self):
+              return hash(("implies",hash(self.antecedent),hash(self.consequent)))
+
+          def __repr__(self):
+               return f"Implication({self.antecedent},{self.consequent})"
+
+           def evaluate(self,model):
+               return ((not self.antecedent.evaluate(model1))
+                       or self.consequent.evaluate(model))
+            def formula(self):
+                antecedent =Sentence.parenthesize(self.antecedent.formula())
+                consequent = Sentence.parenthesize(self.consequent.formula())
+                return f"{antecedent} =>{consequent}"
+
+
+             def symbols(self):
+                 return set.union(self.antecedent.symbols(),self.consequent.symbols())
+class Bicoditional(Sentence):
+     def __init__(self,left,right):
+         Sentence.validate(left)
+         Sentence.validate(right)
+         self.left=left
+         self.right=right
+
+     def __eq__(self,other):
+          return  (isinstance(other,Biconditional)
+                and self.left == other.left
+                and self.right == other.right)
+
+
+       def __hash__(self):
+           return hash(("biconditional",hash(self.left), hash(self.right)))
+
+        def __repr__(self):
+             return  f"Biconditional({self.left},{self.rigth})"
+
+        def evaluate(self,model):
+             return(( self.left.evaluate(model)
+                    and self.right.evaluate(model))
+                    or (not self.left.evaluate(model)
+                       and not self.right.evaluate(model)))
+          def formula(self):
+               left =Sentence.parenthesize(str(self.left))
+               right = Sentence.parenthesize(str(self.right))
+               return f"{left} <=> {right}"
+           def symbols(self):
+                return set.union(self.symbols(), self.right.symbols())
+
+def model_check(knowledge,query):
+   """Checks if knowledge base entails query,given a particular model."""
+
+
+   if not symbols:
+
+          if knowledge.evaluate(model):
+              return query.evalute(model)
+          return  True
+     else:
+
+
+          remaining  = symbols.copy()
+          p = remaining.pop()
+
+          model_true=model.copy()
+          model_true[p] = True
+
+          model_false=model.copy()
+          model_flse[p]=False
+
+          return  (check_all(knowledge,query,remaining,model_true)  and
+                   check_all(knowledge,query,remaining,model_false))
+
+        symbols =set.union(knowledge.symbols(),query.symbols())
+
+        return check_all(knowledge,query,symbols,dict())
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                
 
        
    
